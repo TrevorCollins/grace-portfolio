992b0f1eb69a65f40a1fe41320c698d4b4c4d3ed
TrevorCollins <tcollinst1i@gmail.com>
2023-12-26 16:39:49 -0600

    Update header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,7 @@ const Footer = () => {
     <div className="p-6 opacity-50 font-serif bg-gray-800 flex flex-row gap-2 justify-center">
       <h1 className="text-xl pr-3 border-r-4">GRACE SNYDER</h1>
       <div className="pl-2 flex justify-center items-center">
-        <p className="border-r pr-2">(913)433-4340</p>
-        <p className="px-2">ants.in.sweaters@gmail.com</p>
+        <p>grace@gracesnyder.com</p>
       </div>
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,26 @@
 import InstagramIcon from "@mui/icons-material/Instagram";
 import { IconButton } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="fixed flex justify-between h-36 w-full px-6 py-8 opacity-90 bg-black z-40 ">
-      <div className="font-serif flex flex-col">
-        <h1 className="text-4xl pb-2">GRACE SNYDER</h1>
+    <div className="fixed flex font-primary font-thin justify-between h-36 w-full px-6 py-8 opacity-90 bg-black z-40 ">
+      <div>
+        {/* <h1 className="text-4xl pb-2">grace snyder</h1>
         <div className="pt-2 flex justify-start">
           <p>grace@gracesnyder.com</p>
-        </div>
+        </div> */}
+        <Image
+          src="/assets/images/gracesnyderbanner.png"
+          alt="Grace Snyder"
+          width={400}
+          height={100}
+        />
       </div>
       <div className="opacity-80 hover:opacity-100 hover:scale-105 transition duration-200 ease-in-out">
         <IconButton className="text-white">
-          <Link href="https://www.instagram.com/goldfished/">
+          <Link target="_blank" href="https://www.instagram.com/goldfished/">
             <InstagramIcon />
           </Link>
         </IconButton>
